0245be95f157732c703c13b484fcd017a63357bc
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-28 00:11:43 -0400

    challenge part

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -3,6 +3,15 @@ package com.dsimon;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BankAccount myAccount = new BankAccount();
+
+        myAccount.setAccountNumber(12345);
+        myAccount.setBalance(12.23);
+        myAccount.setCustomerName("Doug");
+        myAccount.setEmail("doug@doug.com");
+        myAccount.setPhoneNumber("123456789");
+        
+        myAccount.depositFunds(23.34);
+        myAccount.withdrawFunds(10.00);
     }
 }
